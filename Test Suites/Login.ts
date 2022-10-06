b8c6ec8f3f373d7e0c21c910789cfd7305b4fb63
Ibnu Fajar Yunardi <ibnufajaryunardi@Ibnu-MacBook-Air.local>
2022-10-06 14:21:07 +0700

    initial

---
+++ Test Suites/Login.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>125576f3-8766-4c9d-8492-3cb3f9b61dff</testSuiteGuid>
+   <testCaseLink>
+      <guid>4fcd97e5-019b-44a4-94d6-ed6090a01f4d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Login/LG001 User Login With Invalid Credential</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>662f04d4-7ac9-4e9a-9906-f13b84edb327</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Steps Definition/Feature Login/LG002 User Login With Valid Credential</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
